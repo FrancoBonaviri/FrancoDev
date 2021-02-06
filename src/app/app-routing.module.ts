76ee8d368ed1d4bb87d48a0581d8e3c70b062aca
Franco <Francobonaviri@hotmail.com>
2021-02-06 20:27:07 -0300

    Primeras implementaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AbautComponent } from './pages/abaut/abaut.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'abaut', component: AbautComponent},
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: '**', redirectTo:'home', pathMatch:'full'},
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AbautComponent } from './pages/abaut/abaut.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AbautComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
